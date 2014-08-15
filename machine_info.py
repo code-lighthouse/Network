23850a4c7dc126b0ec6e261e7d1700c41e22fb6b
Cristiano Araujo <code.lighthouse@gmail.com>
2014-08-15 12:43:33 -0300

    Update machine_info.py

--- machine_info.py
+++ machine_info.py
@@ -8,7 +8,7 @@ def print_machine_info():
     host_name = socket.gethostname()
     ip_adress = socket.gethostbyname(host_name)
     print('Host name: %s' %host_name)
-    print('IP adress: %s' %ip_adress))
+    print('IP adress: %s' %ip_adress)
 
 if __name__ == '__main__':
     print_machine_info()
