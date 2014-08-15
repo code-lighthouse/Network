b39bec269d24eb2b41a08a08c75b7505b5c06234
Cristiano Araujo <code.lighthouse@gmail.com>
2014-08-15 12:40:10 -0300

    Update and rename local_net.py to localhost.py

---
+++ localhost.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+import socket
+
+# Shows the Local Machine's Information (Hostname and IP Adress)
+
+def print_machine_info():
+    host_name = socket.gethostname()
+    ip_adress = socket.gethostbyname(host_name)
+    print('Host name: %s' %host_name)
+    print('IP adress: %s' %ip_adress))
+
+if __name__ == '__main__':
+    print_machine_info()
