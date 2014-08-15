e500fcd61de29ab60aba33bf597268d70ce3102d
Cristiano Araujo <code.lighthouse@gmail.com>
2014-08-15 13:20:41 -0300

    Update local_machine_info.py

--- local_machine_info.py
+++ local_machine_info.py
@@ -2,14 +2,17 @@
 
 import socket
 
-def print_machine_info():
-    '''Shows the Local Machine's Information 
-    (Hostname and IP Adress)'''
-       
-    host_name = socket.gethostname()
-    ip_adress = socket.gethostbyname(host_name)
-    print('Host name: %s' %host_name)
-    print('IP adress: %s' %ip_adress)
+class LocalMachineInfo(object):
+    def __init__(self):
+        ''' Shows the Local Machine's Information
+        (Hostname and IP adress)'''
+
+        host_name = socket.gethostname()
+        ip_adress = socket.gethostbyname(host_name)
+        print('Host name: %s' %host_name)
+        print('IP adress: %s' %ip_adress)
 
 if __name__ == '__main__':
-    print_machine_info()
+    LocalMachineInfo()
+
+
