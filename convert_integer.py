69af5aa705ff2705ab721d8d145f0a6b9e7f0013
Cristiano Araujo <code.lighthouse@gmail.com>
2014-08-15 21:34:32 -0300

    Create convert_integer.py

---
+++ convert_integer.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+import socket
+
+def convert_integer():
+    data = 1234
+    # 32 - bit
+    print('Original: %s => Long host order: %s, Network byte order: %s'
+% (data, socket.ntohl(data), socket.htonl(data)))
+    # 16 - bit
+    print('Original: %s => Short host byte order: %s, Network byte order: %s'
+% (data, socket.ntohl(data), socket.htonl(data)))
+
+if __name__ == '__main__':
+    convert_integer()
